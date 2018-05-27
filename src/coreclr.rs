41e6339c76d71a174759eca415fce5ce999ca2ae
Kevin Jones <kevin@vcsjones.com>
2018-05-27 17:52:55 -0400

    CLR lives with the CLR type.

--- src/coreclr.rs
+++ src/coreclr.rs
@@ -11,34 +11,32 @@ use std::vec::{Vec};
 
 type CoreCLRInitialize = extern "C" fn (*const c_char, *const c_char, c_int, *const *mut c_char, *const *mut c_char, *mut *mut c_void, *mut c_uint) -> c_int;
 type CoreCLRCreateDelegate = extern "C" fn(*mut c_void, c_uint, *const c_char, *const c_char, *const c_char, *mut *mut c_void) -> c_int;
-
-pub struct CoreCLR {
-    library: lib::Library
-}
+type CoreCLRShutdown = extern "C" fn(*mut c_void, c_uint) -> c_int;
 
 #[derive(Debug)]
-pub struct Runtime {
+pub struct CoreCLR {
+    library: lib::Library,
     domain_id : c_uint,
     host_handle : *mut c_void
 }
 
-impl CoreCLR {
-    pub fn new(library_path : &str) -> io::Result<CoreCLR> {
-        let library = lib::Library::new(library_path)?;
-        Ok(CoreCLR {
-            library: library
-        })
+impl Drop for CoreCLR {
+    fn drop(&mut self) {
+        let shutdown_func: lib::Symbol<CoreCLRShutdown> = unsafe { self.library.get(b"coreclr_shutdown").unwrap() };
+        shutdown_func(self.host_handle, self.domain_id);
     }
+}
 
-    pub fn coreclr_createdelegate(&self, runtime : &Runtime, assembly_name : &str, type_name : &str, method_name : &str) -> io::Result<fn() -> ()> {
+impl CoreCLR {
+    pub fn coreclr_createdelegate(&self, assembly_name : &str, type_name : &str, method_name : &str) -> io::Result<unsafe extern fn() -> ()> {
         let create_delegate: lib::Symbol<CoreCLRCreateDelegate> = unsafe { self.library.get(b"coreclr_create_delegate")? };
         let assembly_name_ptr = CString::new(assembly_name)?;
         let type_name_ptr = CString::new(type_name)?;
         let method_name_ptr = CString::new(method_name)?;
         let mut delegate_handle = ptr::null_mut();
         let result = create_delegate(
-            runtime.host_handle,
-            runtime.domain_id,
+            self.host_handle,
+            self.domain_id,
             assembly_name_ptr.as_ptr(),
             type_name_ptr.as_ptr(),
             method_name_ptr.as_ptr(),
@@ -48,11 +46,12 @@ impl CoreCLR {
             Result::Err(Error::from_raw_os_error(result))
         }
         else {
-            Ok(unsafe { mem::transmute::<*mut c_void, fn() -> ()>(delegate_handle) })
+            Ok(unsafe { mem::transmute::<*mut c_void, unsafe extern fn() -> ()>(delegate_handle) })
         }
     }
 
-    pub fn coreclr_initialize(&self, exe_path : &str, app_domain_friendly_name : &str, properties : HashMap<&str, &str>) -> io::Result<Runtime> {
+    pub fn new(library_path : &str, exe_path : &str, app_domain_friendly_name : &str, properties : HashMap<&str, &str>) -> io::Result<CoreCLR> {
+        let library = lib::Library::new(library_path)?;
         let exe_path_ptr = CString::new(exe_path)?;
         let app_domain_friendly_name_ptr = CString::new(app_domain_friendly_name)?;
         let mut keys = Vec::with_capacity(properties.len());
@@ -63,16 +62,18 @@ impl CoreCLR {
         };
         let mut host_handle = ptr::null_mut();
         let mut domain_id = 0;
-        let initializer: lib::Symbol<CoreCLRInitialize> = unsafe { self.library.get(b"coreclr_initialize")? };
-        let result = initializer(
-            exe_path_ptr.as_ptr(),
-            app_domain_friendly_name_ptr.as_ptr(),
-            properties.len() as i32,
-            keys.as_ptr(),
-            values.as_ptr(),
-            &mut host_handle,
-            &mut domain_id
-        );
+        let result = {
+            let initializer: lib::Symbol<CoreCLRInitialize> = unsafe { library.get(b"coreclr_initialize")? };
+            initializer(
+                exe_path_ptr.as_ptr(),
+                app_domain_friendly_name_ptr.as_ptr(),
+                properties.len() as i32,
+                keys.as_ptr(),
+                values.as_ptr(),
+                &mut host_handle,
+                &mut domain_id
+            )
+        };
 
         // We need to call from_raw on our original C strings because into_raw
         // forgets them. If we don't, we leak the memory in keys and values.
@@ -86,7 +87,11 @@ impl CoreCLR {
             Result::Err(Error::from_raw_os_error(result))
         }
          else {
-            Result::Ok(Runtime { domain_id : domain_id, host_handle : host_handle })
+            Result::Ok(CoreCLR {
+                domain_id : domain_id,
+                host_handle : host_handle,
+                library : library
+            })
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap};
 
 
 fn main() {
-    let coreclr = coreclr::CoreCLR::new("/the/path/to/libcoreclr.dylib").unwrap();
+    
     let exe_path = "<path to the container folder of your assemblies>";
     let app_domain = "rusty";
     let assembly_name = "MyAssembly";
@@ -13,7 +13,7 @@ fn main() {
     let mut map = HashMap::new();
     map.insert("APP_PATHS", exe_path);
     map.insert("TRUSTED_PLATFORM_ASSEMBLIES", "<list of trusted platform assemblies>");
-    let runtime = coreclr.coreclr_initialize(exe_path, app_domain, map).unwrap();
-    let managed_function = coreclr.coreclr_createdelegate(&runtime, assembly_name, type_name, entry_point).unwrap();
-    managed_function();
+    let coreclr = coreclr::CoreCLR::new("/the/path/to/libcoreclr.dylib", exe_path, app_domain, map).unwrap();
+    let managed_function = coreclr.coreclr_createdelegate(assembly_name, type_name, entry_point).unwrap();
+    unsafe { managed_function() };
 }
