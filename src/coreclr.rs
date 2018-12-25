5ce2af10e90a5ec3ab0329e82dad4f16895eb47b
Kevin Jones <kevin@vcsjones.com>
2018-12-25 11:03:55 -0500

    Update to Rust 2018.

--- src/coreclr.rs
+++ src/coreclr.rs
@@ -1,9 +1,7 @@
-extern crate libloading as lib;
-
 use std::collections::{HashMap};
 use std::ffi::{CString};
-use std::{io, mem, ptr};
-use std::io::{Error, ErrorKind};
+use std::{mem, ptr};
+use std::io::{Error, ErrorKind, Result};
 use std::marker::{PhantomData};
 use std::ops::{Deref};
 use std::os::raw::{c_int, c_char, c_uint, c_void};
@@ -28,7 +26,7 @@ impl<'clr, T> Deref for ManagedDelegate<'clr, T> {
 
 #[derive(Debug)]
 pub struct CoreCLR {
-    library: lib::Library,
+    library: libloading::Library,
     domain_id : c_uint,
     host_handle : *mut c_void
 }
@@ -38,15 +36,15 @@ impl Drop for CoreCLR {
         // If we were able to successfully call coreclr_initialize, it's reasonable to assume we should be able to
         // shut it down. Panicking if the entry point can't be found is the best we can do since the trait doesn't
         // let us do anything else.
-        let shutdown_func: lib::Symbol<CoreCLRShutdown> = unsafe { self.library.get(b"coreclr_shutdown").unwrap() };
+        let shutdown_func: libloading::Symbol<CoreCLRShutdown> = unsafe { self.library.get(b"coreclr_shutdown").unwrap() };
         let result = shutdown_func(self.host_handle, self.domain_id);
         debug_assert!(result >= 0);
     }
 }
 
 impl CoreCLR {
-    pub fn create_delegate<'clr, T: 'clr>(&'clr self, assembly_name : &str, type_name : &str, method_name : &str) -> io::Result<ManagedDelegate<'clr, T>> {
-        let create_delegate: lib::Symbol<CoreCLRCreateDelegate> = unsafe { self.library.get(b"coreclr_create_delegate")? };
+    pub fn create_delegate<'clr, T: 'clr>(&'clr self, assembly_name : &str, type_name : &str, method_name : &str) -> Result<ManagedDelegate<'clr, T>> {
+        let create_delegate: libloading::Symbol<CoreCLRCreateDelegate> = unsafe { self.library.get(b"coreclr_create_delegate")? };
         let assembly_name_ptr = CString::new(assembly_name)?;
         let type_name_ptr = CString::new(type_name)?;
         let method_name_ptr = CString::new(method_name)?;
@@ -60,19 +58,19 @@ impl CoreCLR {
             &mut delegate_handle
         );
         if result < 0 {
-            Result::Err(Error::from_raw_os_error(result))
+            Err(Error::from_raw_os_error(result))
         }
         else {
             Ok(ManagedDelegate { managed_pointer: delegate_handle, phantom: PhantomData })
         }
     }
 
-    pub fn new(library_path : &str, exe_path : &str, app_domain_friendly_name : &str, properties : &HashMap<&str, &str>) -> io::Result<CoreCLR> {
+    pub fn new(library_path : &str, exe_path : &str, app_domain_friendly_name : &str, properties : &HashMap<&str, &str>) -> Result<CoreCLR> {
         static INITIALIZED: AtomicBool = ATOMIC_BOOL_INIT;
         if INITIALIZED.compare_and_swap(false, true, Ordering::Relaxed) {
             return Err(Error::new(ErrorKind::Other, "The CLR cannot be loaded more than once. See dotnet/coreclr#12266."));
         }
-        let library = lib::Library::new(library_path)?;
+        let library = libloading::Library::new(library_path)?;
         let exe_path_ptr = CString::new(exe_path)?;
         let app_domain_friendly_name_ptr = CString::new(app_domain_friendly_name)?;
         let mut keys = Vec::with_capacity(properties.len());
@@ -84,7 +82,7 @@ impl CoreCLR {
         let mut host_handle = ptr::null_mut();
         let mut domain_id = 0;
         let result = {
-            let initializer: lib::Symbol<CoreCLRInitialize> = unsafe { library.get(b"coreclr_initialize")? };
+            let initializer: libloading::Symbol<CoreCLRInitialize> = unsafe { library.get(b"coreclr_initialize")? };
             initializer(
                 exe_path_ptr.as_ptr(),
                 app_domain_friendly_name_ptr.as_ptr(),
@@ -105,10 +103,10 @@ impl CoreCLR {
             let _ = unsafe { CString::from_raw(value) };
         };
         if result < 0 {
-            Result::Err(Error::from_raw_os_error(result))
+            Err(Error::from_raw_os_error(result))
         }
          else {
-            Result::Ok(CoreCLR {
+            Ok(CoreCLR {
                 domain_id : domain_id,
                 host_handle : host_handle,
                 library : library
